ce265dc5c4fcc682de72972d7d01aaf2d7e67470
Tom Li <tom@qvivo.com>
2012-10-24 10:03:43 +0800

    - revert changes made to UIViewAdapter

--- UIKit/Classes/UIViewAdapter.m
+++ UIKit/Classes/UIViewAdapter.m
@@ -81,9 +81,6 @@
 
 - (void)_addNSView
 {
-    //deal with 10.8 geometry differences.
-    self.geometryFlipped = [[[[containerView window].screen UIKitView] layer] isGeometryFlipped];
-
     [_clipView scrollToPoint:NSPointFromCGPoint(self.contentOffset)];
     
     [[self.window.screen UIKitView] addSubview:_clipView];
